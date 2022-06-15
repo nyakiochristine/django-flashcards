1a5836fdef720f4279d22ca714c0b158185716e2
Geb54g <gabriel.ndolo@student.moringaschool.com>
2022-06-15 15:38:40 +0300

    ft:created views for the app

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def indexView(request):
+    return render(request,'index.html')
+
+def dashboardViews(request):
+    return render(request,'dashboard.html')
+
+def registerViews(request):
+    return render(request,'registration/register.html')
