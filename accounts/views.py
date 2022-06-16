bd6dfc1f9b465a74a7486798ef279afe1d3de439
Geb54g <gabriel.ndolo@student.moringaschool.com>
2022-06-16 06:58:30 +0300

    ft:created form authentication

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def indexView(request):
     return render(request,'index.html')
 
-def dashboardViews(request):
+def dashboardView(request):
     return render(request,'dashboard.html')
 
-def registerViews(request):
+def registerView(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login_url')
+    else:
+        form = UserCreationForm()
     return render(request,'registration/register.html')
