bf3fdbf43881dfd6642a433e7da6c08004439a61
Christine Mwangi <christine.mwangi@student.moringaschool.com>
2022-06-20 07:57:35 +0300

    ft:created a view function for search bar

--- cards/views.py
+++ cards/views.py
@@ -1,9 +1,14 @@
-from django.shortcuts import render
-from django.http import HttpResponse,
+from django.shortcuts import (
+    HttpResponseRedirect,
+	get_object_or_404,
+    render
+)
+from django.urls import reverse
 from django.contrib.auth.forms import UserCreationForm
 from django.forms import ModelForm
 from .forms import Card_Set_Form,Card_Form
 from .models import Card,Card_Set
+from PyDictionary import PyDictionary 
 # Create your views here.
 def flashcards(request):
     '''
@@ -12,7 +17,7 @@ def flashcards(request):
     # Card_Set.objects.all() returns all cards in the database
     topic_query_set = Card_Set.objects.all().order_by('topic').filter(is_active = True)
     context = {'topics': topic_query_set}   # Dictionary{} rendered for flashcards/flashcards.html
-    return render(request, 'flashcards/flashcards.html', context)
+    return render(request, 'flashcards.html', context)
 
 
 def create_card_set(request):
@@ -25,7 +30,7 @@ def create_card_set(request):
 
         if form.is_valid():                                               # Check if form is valid
             form.save()                                                   # Save form - object saved to database
-            return HttpResponseRedirect(reverse('flashcards:flashcards')) # Redirect to the flashcards view
+            return HttpResponseRedirect (reverse('flashcards:flashcards')) # Redirect to the flashcards view
 
     else:
         if request.user.is_authenticated:
@@ -34,7 +39,7 @@ def create_card_set(request):
             form = Card_Set_Form()
 
     context = {'form': form}
-    return render(request, 'flashcards/create_card_set.html', context)
+    return render(request, 'create_card_set.html', context)
 
 
 def create_card(request, card_set_id):
@@ -55,7 +60,7 @@ def create_card(request, card_set_id):
         form = Card_Form(initial = {'parent_card_set': card_set_object})
 
     context = {'form': form}
-    return render(request, 'flashcards/add_edit_cards.html', context)
+    return render(request, 'add_edit_cards.html', context)
 
 
 def delete_card(request, card_id):
@@ -93,7 +98,7 @@ def edit_card(request, card_id):
         form = Card_Form(instance = card_object)
 
     context = {'form': form, 'edit_mode': True, 'card_object': card_object}
-    return render(request, 'flashcards/add_edit_cards.html', context)
+    return render(request, 'add_edit_cards.html', context)
 
 
 def edit_card_set(request, card_set_id):
@@ -114,7 +119,7 @@ def edit_card_set(request, card_set_id):
 		form = Card_Set_Form(instance = card_set_object)
 
 	context = {'form': form, 'edit_mode': True, 'card_set_object': card_set_object}
-	return render(request, 'flashcards/create_card_set.html', context)
+	return render(request, 'create_card_set.html', context)
 
 
 def view_card_set(request, card_set_id):
@@ -132,6 +137,28 @@ def view_card_set(request, card_set_id):
         card_object = get_object_or_404(Card, id = request.GET['card'])
 
     context = {'card_set_object': card_set_object, 'card_object': card_object}
-    return render(request, 'flashcards/view_cards.html', context)
+    return render(request, 'view_cards.html', context)
 
+
+
+def dictionary(request):
+    '''
+    View for the 'search bar' in the
+    navbar - a page rendered that
+    displaying the searched word and its
+    definition using PyDictionary
+    '''
+
+    try:
+        if request.method == 'GET':
+            word_searched = request.GET.get('word_searched')
+            dictionary = PyDictionary()                  # PyDictionary 
+            word_def = dictionary.meaning(word_searched) # Get definition of word searched
+            word_def = list(word_def.values())[0][0]     # get definition as a string
+
+    except:
+        word_def = 'This word does not exist in the database'
+
+    context = {'word_searched': word_searched, 'word_def': word_def}
+    return render(request, 'diction.html', context)
 
