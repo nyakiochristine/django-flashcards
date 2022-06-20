ffe7e15b0582d8d2f84a529d8d5fe8b8c584aca2
Christine Mwangi <christine.mwangi@student.moringaschool.com>
2022-06-20 08:44:12 +0300

    ft:registered models

--- cards/admin.py
+++ cards/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 # Register your models here.
 from .models import Card_Set,Card
 
-admin.register(Card)
-admin.register(Card_Set)
+admin.site.register(Card)
+admin.site.register(Card_Set)
 
